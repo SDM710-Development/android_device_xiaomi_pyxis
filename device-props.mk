33b8f4f16c04a0bd86311f8de77abea794d783a2
Bruno Martins <bgcngm@gmail.com>
2018-09-21 17:01:31 +0100

    ursa: Add stock system props
    Change-Id: I562556f0289aad2e2567cd9a3fdf8515f869e213

---
+++ device-props.mk
@@ -0,0 +1,17 @@
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.sat.fallback.dist=45 \
+    persist.camera.sat.fallback.dist.d=5 \
+    persist.camera.sat.fallback.luxindex=310 \
+    persist.camera.sat.fallback.lux.d=50
+
+# Display features
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.displayfeature.histogram.enable=false \
+    ro.eyecare.brightness.threshold=11 \
+    ro.eyecare.brightness.level=8 \
+    ro.qcom.ad=1 \
+    ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
+    ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
+    ro.qcom.ad.sensortype=2 \
+    ro.qualcomm.cabl=0
