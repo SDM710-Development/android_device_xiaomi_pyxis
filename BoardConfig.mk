LineageOS/android_device_xiaomi_ursa

ursa: Set tap to wake node path
Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>
Change-Id: Ibb27fce9f0ced35c11b7b8625bb797d449e17916

BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_USES_NQ_NFC := true
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event2"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 

SDM710-Development/android_device_xiaomi_pyxis

BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_USES_NQ_NFC := true
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 