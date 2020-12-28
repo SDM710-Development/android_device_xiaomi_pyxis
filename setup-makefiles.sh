a265e78bf7293e11196eb81c664df6f61fcfdcae
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    pyxis: Align with extraction-utils templates
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I4a33a129bcf0e82903567361cbbf01072c4bf280

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Required!
 export DEVICE=pyxis
 export DEVICE_COMMON=sdm710-common
 export VENDOR=xiaomi
 
-export DEVICE_BRINGUP_YEAR=2019
-
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"../../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
