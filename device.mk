8932682b74eb6159edd283f5a00fff59ad5947d0
Ivan Vecera <ivan@cera.cz>
2021-01-26 15:03:31 +0100

    pyxis: add support for IR
    TODO add sepolicy
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ibe6ef180567ac1ea4b49246a17dc36c716d6c6e4

--- device.mk
+++ device.mk
@@ -83,6 +83,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_sdm710
