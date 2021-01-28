c9f666f2764ce692798b1fd8bb7075141bfa8617
Ivan Vecera <ivan@cera.cz>
2021-01-28 16:58:12 +0100

    pyxis: add consumerir feature
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Icc18d99a7d31c0b4834ef257af581d19514e615c

--- device.mk
+++ device.mk
@@ -29,6 +29,7 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
