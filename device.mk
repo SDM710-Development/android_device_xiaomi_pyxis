5f5d1fbdf5803b59840f24fe09c6a7f9032cc4a9
Ivan Vecera <ivan@cera.cz>
2021-02-02 17:40:21 +0100

    pyxis: import device specific sensor config
    Inspired from commit 3c1e05c44ec1b3cdde5eabb6409bd1b2f111741e from
    sirius and adjusted for this device tree.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Iaaa2c065f8694c8596ac789044ac19b779ed1cab

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SecureElement
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     vendor/nxp/opensource/pn5xx
