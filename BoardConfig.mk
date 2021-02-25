5aed9590038f0a0a01ba1970162092468b12a3d5
Ivan Vecera <ivan@cera.cz>
2021-02-25 13:09:12 +0100

    pyxis: switch selinux to enforcing
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I7428220fe47a2488c09712c514ebc279b091a4ca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,6 @@ TARGET_SCREEN_DENSITY := 440
 # Kernel
 TARGET_KERNEL_CONFIG := pyxis_defconfig
 BOARD_KERNEL_SEPARATED_DTBO := true
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
