7afe4b287325e7ab36b834fef7489f875ded0074
Ivan Vecera <ivan@cera.cz>
2021-03-19 17:08:31 +0100

    pyxis: fix extract-files.sh
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- extract-files.sh
+++ extract-files.sh
@@ -18,4 +18,4 @@ export DEVICE=pyxis
 export DEVICE_COMMON=sdm710-common
 export VENDOR=xiaomi
 
-"../../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
