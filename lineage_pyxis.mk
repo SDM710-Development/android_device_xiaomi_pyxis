28e3543ab2fc4dbcdf3b08ca04ac4816e7200b11
Yuri Konotopov <ykonotopov@gnome.org>
2021-06-28 06:08:36 +0000

    Update build fingerprint to latest global stable ROM

--- lineage_pyxis.mk
+++ lineage_pyxis.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V12.0.6.0.QFCMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/pyxis/pyxis:10/QKQ1.190828.002/V12.0.7.0.QFCMIXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="pyxis-user 11 RKQ1.200826.002 V12.5.1.0.RFCCNXM release-keys" \
