33663a855aeff3115e293d1af124dcb2883cc9c6
Ivan Vecera <ivan@cera.cz>
2021-11-08 12:54:33 +0100

    pyxis: gps: Remove gps_debug.conf
    SUPL and XTRA configuration is present in sdm710-common so
    it is not necessary to override it here.
    
    Fixes:
    "build/make/core/Makefile:49: warning: overriding commands for target `out/target/product/pyxis/system/etc/gps_debug.conf'
    "
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ie2cddfa0ac28f237051979238e5d02dd1405baaf

--- device.mk
+++ device.mk
@@ -75,10 +75,6 @@ PRODUCT_PACKAGES += \
 # FM
 BOARD_HAVE_QCOM_FM := true
 
-# GPS
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps_debug.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_sdm710
