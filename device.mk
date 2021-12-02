878435a10d9f4e44b2a1f2c3981e3bf84d222319
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-02 00:51:41 +0000

    pyxis: Put common tree inheritting on top
    Change-Id: I2f6541d7f2f9c223e5a945d999a7f5e8423336e3

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sdm710-common
+$(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
@@ -41,9 +44,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
-
-# Inherit from sdm710-common
-$(call inherit-product, device/xiaomi/sdm710-common/sdm710.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
